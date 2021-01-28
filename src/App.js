9990f50d525d552701ff0f6ef40b2cb9b7c28ef5
Devin Ford <devindford@att.net>
2021-01-28 02:36:18 -0500

    fixed?

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './App.css';
 import { ProvideAuth } from './library/auth';
 import MainLayout from './components/MainLayout';
+import 'firebase/analytics';
+import firebase from 'firebase/app';
+//eslint-disable-next-line
+const defaultAnalytics = firebase.analytics();
 
 function App() {
   return (
